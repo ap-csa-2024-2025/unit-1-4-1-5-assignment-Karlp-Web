d557d48a165c7ce89a25c38abebb2faecc23c20b
Karlp-Web <Karlenportillo1@gmail.com>
2024-09-10 10:39:49 -0700

    yomomma

--- Problem0.java
+++ Problem0.java
@@ -2,6 +2,11 @@ public class Problem0
 {
   public static void main(String[] args)
   {
-    // insert code here
+    int money = 2.00;
+
+    double amountOfBills = 5;
+
+    System.out.print(money);
+    System.out.print(amountOfBills);
   }
 }
