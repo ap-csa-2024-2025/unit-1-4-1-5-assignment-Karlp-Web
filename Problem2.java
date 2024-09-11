251bc07b5abf826a6b90a4e4ec7761823e493756
Karlp-Web <Karlenportillo1@gmail.com>
2024-09-11 10:47:27 -0700

    yomomma

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,17 @@
+public class Problem2
+{
+  public static void main(String[] args)
+  {
+    int value = 498;
 
+    System.out.println("Here are the three digits:");
+
+    System.out.println(value % 494);
+
+    System.out.println((value % 482) /2);
+
+    System.out.println(value / 55);
+
+
+  }
+}
